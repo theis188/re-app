3e3bbdc15b588e2154484799460edb22958e92db
Matthew Theisen <mtheisen@ucla.edu>
2016-10-11 16:12:47 -0700

    Classifying Neighborhoods

--- getcenters.py
+++ getcenters.py
@@ -1,6 +1,10 @@
 import os
 import ast
 from shapely.geometry import Polygon, Point
+from getscores import getrank
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+import numpy as np
 
 basepath = 'Coords/'
 coordl = {}
@@ -39,4 +43,74 @@ def findnearest(name,n):
 	sort = sorted(dists)
 	return [j[1] for i,j in enumerate(sort) if i in range(1,n+1)]
 
-print findnearest('santa-monica',10)
+def mean(arr):
+	return sum([float(i) for i in arr])/float(len(arr))
+
+avcols = ['viol-crime','school-qual','prop-crime','air-qual']
+# avcols = ["pop-dens-h","rest-dens","prop-crime"]
+pricecol = ['buy-price']
+
+avscores = {name: float(scorey) for (name,scorey) in zip(goodnames,getrank(avcols))}
+pscores = {name: float(scorey) for (name,scorey) in zip(goodnames,getrank(pricecol))}
+# print findnearest('central-alameda',5)
+# ne = 'central-alameda'
+# print 'overall score',avscores[ne]
+# print 'price score',pscores[ne]
+# print 'nearest 5',findnearest(ne,5)
+# print 'nearest 5 overall scores',[ avscores[name] for name in findnearest(ne,5) ]
+# print 'nearest 5 price scores',[ avscores[name] for name in findnearest(ne,5) ]
+#print pscores
+diffav = {}
+diffp = {}
+for ne in goodnames:
+	narray = findnearest(ne,10)
+	neavsc = float(avscores[ne])
+	nepsc = float(pscores[ne])
+	nearavsc = mean( [ avscores[name] for name in narray ] )
+	nearpsc = mean( [ pscores[name] for name in narray ] )
+	diffav[ne] = neavsc-nearavsc
+	diffp[ne] = nepsc-nearpsc
+	# print ne,neavsc,'rank score vs',nearavsc,'for nearby'
+	# print ne,nepsc,'price score vs',nearpsc,'for nearby'
+
+	# aaa = raw_input('aaa')
+
+def onpick3(event):
+	ind = event.ind
+	print 'neighborhood:', goodnames[ind]
+
+
+x=diffav.values()
+y=diffp.values()
+
+# x=[avscores[name] for name in goodnames]
+# y=[pscores[name] for name in goodnames]
+
+xx = np.array(x).reshape( (155,1) )
+yy = np.array(y).reshape( (155,1) )
+
+print xx.shape
+print yy.shape
+
+regr = linear_model.LinearRegression()
+regr.fit(xx,yy)
+print regr.score(xx,yy)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+col = ax.scatter(x, y, picker=True)
+plt.xlabel('Other-Rank Difference (neighborhood-nearby)')
+plt.ylabel('Price-Rank Difference (neighborhood-nearby)')
+fig.canvas.mpl_connect('pick_event', onpick3)
+plt.show()
+
+
+
+x=avscores.values()
+y=pscores.values()
+
+ax = plt.subplot()
+ax.plot(x,y,linestyle='None',marker='o')
+plt.xlabel('Other-Rank')
+plt.ylabel('Price-Rank')
+plt.show()
