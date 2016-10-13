bac8f6b5a668a9a50ea94d8d8ea9a1d16252134e
mtheisen@ucla.edu <mtheisen@ucla.edu>
2016-10-13 06:31:04 -0700

    linreg

--- getcenters.py
+++ getcenters.py
@@ -46,7 +46,7 @@ def findnearest(name,n):
 def mean(arr):
 	return sum([float(i) for i in arr])/float(len(arr))
 
-avcols = ['viol-crime','school-qual','prop-crime','air-qual']
+avcols = ['viol-crime','school-qual','air-qual']
 # avcols = ["pop-dens-h","rest-dens","prop-crime"]
 pricecol = ['buy-price']
 
@@ -80,27 +80,49 @@ def onpick3(event):
 	print 'neighborhood:', goodnames[ind]
 
 
-x=diffav.values()
-y=diffp.values()
+# x=diffav.values()
+# y=diffp.values()
 
-# x=[avscores[name] for name in goodnames]
-# y=[pscores[name] for name in goodnames]
+x=[avscores[name] for name in goodnames]
+y=[pscores[name] for name in goodnames]
 
-xx = np.array(x).reshape( (155,1) )
-yy = np.array(y).reshape( (155,1) )
+xy = [(xi,yi) for xi,yi in zip(x,y) if xi > 20]
+print zip(xy)
+
+xx,yy = zip(*xy)
+
+
+xx = np.array(xx).reshape( (154,1) )
+yy = np.array(yy).reshape( (154,1) )
+
+# xx = xx[xx>20].reshape( (154,1) )
+# yy = yy[xx>20].reshape( (154,1) )
 
 print xx.shape
 print yy.shape
 
 regr = linear_model.LinearRegression()
-regr.fit(xx,yy)
-print regr.score(xx,yy)
+
+# print xx,xx**2
+xxx = np.concatenate((xx,xx**2),1)
+regr.fit(xxx,yy)
+
+print xxx.shape
+print regr.score(xxx,yy)
+m1=regr.coef_[0][0]
+m2=regr.coef_[0][1]
+b=regr.intercept_[0]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 col = ax.scatter(x, y, picker=True)
-plt.xlabel('Other-Rank Difference (neighborhood-nearby)')
-plt.ylabel('Price-Rank Difference (neighborhood-nearby)')
+
+pltx = [1.0 + i for i in range(20,95)]
+plty = [m1*x + m2*(x**2) + b for x in pltx]
+
+ax.plot(pltx,plty,c='black',lw=2)
+plt.xlabel('Other-Rank ')
+plt.ylabel('Price-Rank ')
 fig.canvas.mpl_connect('pick_event', onpick3)
 plt.show()
 
